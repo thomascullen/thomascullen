25d2079ebd901a8a11ea367d004f783d29525300
Thomas Cullen <thomascullen92@gmail.com>
2016-06-11 12:38:48 +0100

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,37 +2,43 @@ var project_cards = document.querySelectorAll('.project_card a')
 const maxRotation = 15;
 const main_image_width = $('.main_image').width();
 
-$('body').on('mousemove', '.project_card a', function(e) {
-  const x = e.pageX - (this.offsetLeft + main_image_width);
-  const y = e.pageY - this.offsetTop;
-
-  var xChange = calculateChange(x, this.clientWidth);
-  var yChange = calculateChange(y, this.clientHeight);
-
-  var xRotation = yChange * maxRotation + "deg";
-  var yRotation = xChange * maxRotation * -1 + "deg";
-
-  $(this).velocity({
-    scale: 1.1,
-    queueu: false,
-    rotateY: yRotation,
-    rotateX: xRotation,
-  }, {
-    duration: 1,
-    queue: false,
-  });
+$('.nav_toggle').click(function() {
+  $('body').toggleClass("nav_open");
 });
 
-$('body').on('mouseout', '.project_card a', function(e) {
-  $(this).velocity({
-    scale: 1,
-    rotateY: 0,
-    rotateX: 0,
-  }, {
-    queue: false,
-    duration: 100,
+if ( $(window).width() > 800 ) {
+  $('body').on('mousemove', '.project_card a', function(e) {
+    const x = e.pageX - (this.offsetLeft + main_image_width);
+    const y = e.pageY - this.offsetTop;
+
+    var xChange = calculateChange(x, this.clientWidth);
+    var yChange = calculateChange(y, this.clientHeight);
+
+    var xRotation = yChange * maxRotation + "deg";
+    var yRotation = xChange * maxRotation * -1 + "deg";
+
+    $(this).velocity({
+      scale: 1.1,
+      queueu: false,
+      rotateY: yRotation,
+      rotateX: xRotation,
+    }, {
+      duration: 1,
+      queue: false,
+    });
   });
-});
+
+  $('body').on('mouseout', '.project_card a', function(e) {
+    $(this).velocity({
+      scale: 1,
+      rotateY: 0,
+      rotateX: 0,
+    }, {
+      queue: false,
+      duration: 100,
+    });
+  });
+}
 
 function calculateChange(offset, dimension) {
   return ((-2 / dimension) * offset) + 1;
